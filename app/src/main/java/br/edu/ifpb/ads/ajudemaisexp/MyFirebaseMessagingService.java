b524670a80be6cc47da36cfd9305aef7eb44778b
Franck Aragão <fjunior.aragao@gmail.com>
2017-02-14 19:45:16 -0300

    Update

--- app/src/main/java/br/edu/ifpb/ads/ajudemaisexp/MyFirebaseMessagingService.java
+++ app/src/main/java/br/edu/ifpb/ads/ajudemaisexp/MyFirebaseMessagingService.java
@@ -1,5 +1,12 @@
 package br.edu.ifpb.ads.ajudemaisexp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,7 +21,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        Log.d("Ajude Mais!", "From: " + remoteMessage.getFrom());
-        Log.d("Ajude Mais!", "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.androidauthority.com/"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        long[] v = {500,1000};
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setSmallIcon(R.drawable.ic_stat_name);
+        mBuilder.setContentTitle("Ajude Mais");
+        mBuilder.setContentText(remoteMessage.getNotification().getBody());
+        mBuilder.setVibrate(v);
+        mBuilder.setSound(uri);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(001, mBuilder.build());
+
     }
 }
